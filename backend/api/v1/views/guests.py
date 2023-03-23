c973a16eb98c48865632a41167d4e7f104eba528
greenbel <obisann@gmail.com>
2023-03-23 11:48:22 +0000

    Implemented fetching specific service details
    GET /services
    - now returns service-provider service id
    
    GET /services/<id>
    Returns in a dict:
    - service description as `description`
    - service provider first name as `first_name`
    - service provider last name as `last_name`
    - URI of the service profile pic as `image_uri`
    - rating for the service as `rating`
    - all reviews for the service in a list of dicts `reviews`

--- backend/api/v1/views/guests.py
+++ backend/api/v1/views/guests.py
@@ -9,6 +9,11 @@ from flask import (
 from flask_login import (
         login_user, logout_user, login_required, current_user
         )
+from api.v1.views import (
+        db, ServiceProviders, ServiceCategories,
+        ServiceProviderServices, Countries, States, Locations,
+        Reviews, Customers
+        )
 from werkzeug.security import check_password_hash
 from uuid import uuid4
 
@@ -45,16 +50,16 @@ def search_services():
             Reviews
             )
     # Retrieve query arguments
-    country_id = request.args.get('country')
+    country_id = request.args.get('country')  # required
     state_id = request.args.get('state')
     location_id = request.args.get('location')
-    sc_id = request.args.get('service_category')
+    sc_id = request.args.get('service_category')  # required
 
     # Use filter parameters to fetch summary services data
     if not location_id:
         if not state_id:
             # use country id to filter
-            stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).join(States).join(Countries).where(Countries.id==int(country_id), ServiceCategories.id==int(sc_id))
+            stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description, ServiceProviderServices.id).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).join(States).join(Countries).where(Countries.id==int(country_id), ServiceCategories.id==int(sc_id))
             rows_list = db.session.execute(stmt).all()
             '''
             - returns a list of Row objects representing the result set
@@ -65,11 +70,11 @@ def search_services():
             '''
         else:
             # use state id to filter
-            stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).join(States).where(States.id==int(state_id), ServiceCategories.id==int(sc_id))
+            stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description, ServiceProviderServices.id).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).join(States).where(States.id==int(state_id), ServiceCategories.id==int(sc_id))
             rows_list = db.session.execute(stmt).all()
     else:
         # use location id to filter
-        stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).where(Locations.id==int(location_id), ServiceCategories.id==int(sc_id))
+        stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description, ServiceProviderServices.id).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).join(Locations).where(Locations.id==int(location_id), ServiceCategories.id==int(sc_id))
         rows_list = db.session.execute(stmt).all()
 
     json_list = []
@@ -81,14 +86,55 @@ def search_services():
         image_uri = row.image_uri
         rating = float(row.rating)  # for serializability
         description = row.service_description
+        sps_id = row.id
 
         # A dictionary representing a single unit of data
-        json_data = dict(first_name=first_name, last_name=last_name, image_uri=image_uri, rating=rating, description=description)
+        json_data = dict(first_name=first_name, last_name=last_name, image_uri=image_uri, rating=rating, description=description, sps_id=sps_id)
 
         # Append to list
         json_list.append(json_data)
 
     return jsonify(json_list)
+
+
+@guest_apis.route('/services/<sps_id>')
+def one_service(sps_id):
+    ''' Returns details about a specific service-provider service.
+    '''
+    # Fetch service details
+    stmt = db.select(ServiceProviders.first_name, ServiceProviders.last_name, ServiceCategories.name, ServiceProviderServices.image_uri, ServiceProviderServices.rating, ServiceProviderServices.service_description).select_from(ServiceProviderServices).join(ServiceCategories).join(ServiceProviders).where(ServiceProviderServices.id==int(sps_id))
+    details_row = db.session.execute(stmt).one()
+
+    # Fetch all reviews for the specified service
+    stmt = db.select(Reviews.review_content, Reviews.updated_at, Customers.first_name, Customers.last_name).join(Customers).join(ServiceProviderServices).where(ServiceProviderServices.id==int(sps_id))
+    reviews_rows_list = db.session.execute(stmt).all()
+    ''' returns list of reviews details rows.'''
+
+    # json_data =
+
+    # Prepare the data in json_serializable forms
+    first_name = details_row.first_name
+    last_name = details_row.last_name
+    image_uri = details_row.image_uri
+    rating = float(details_row.rating)  # for serializability
+    description = details_row.service_description
+
+    json_data = dict(first_name=first_name, last_name=last_name, image_uri=image_uri, rating=rating, description=description)
+
+    # Prepare list of reviews and associated details
+    reviews = []
+    for reviews_row in reviews_rows_list:
+        content = reviews_row.review_content
+        updated_at = reviews_row.updated_at.isoformat()  # make serializable
+        first_name = reviews_row.first_name
+        last_name = reviews_row.last_name
+        review_json = dict(content=content, customer_first_name=first_name, customer_last_name=last_name, updated_at=updated_at)
+        reviews.append(review_json)
+
+    # Add reviews list to return data
+    json_data.update({"reviews": reviews})
+
+    return jsonify(json_data)
 
 
 
