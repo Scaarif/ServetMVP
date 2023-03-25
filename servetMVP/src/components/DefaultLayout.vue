118e7b3091fb782736c1f7db1745f7bc9b40a69e
scaarifintl <scaarifintl@gmail.com>
2023-03-25 16:31:45 +0300

    solve merge conflicts with coldplayz

--- servetMVP/src/components/DefaultLayout.vue
+++ servetMVP/src/components/DefaultLayout.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-50 min-h-screen h-full relative">
+    <div class="bg-gray-50 min-h-screen h-full relative text-slate-800">
         <Navbar v-if="!isLanding"/>
         <main class="mx-4 top-16 relative flex flex-col items-center">
             <router-view />
