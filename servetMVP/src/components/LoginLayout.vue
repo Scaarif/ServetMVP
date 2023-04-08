a367992faf67d2b8a10be3e2377f5407fac2a082
greenbel <obisann@gmail.com>
2023-04-08 10:25:31 +0000

    First commit

--- servetMVP/src/components/LoginLayout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="flex flex-col items-center justify-center min-h-screen h-full bg-gray-50">
-        <router-view />
-    </div>
-</template>
--- servetMVP/src/config/index.js
+++
@@ -1,9 +0,0 @@
-const ENDPOINT = 'http://localhost:5000/api/v1'
-
-// export the endpoints (eg SERVICES, LOGIN, etc)
-export default {
-    DEFAULT: ENDPOINT,
-    SERVICES: ENDPOINT.concat('/services'),
-    CUSTOMERS: ENDPOINT.concat('/customers/'),
-    PROVIDERS: ENDPOINT.concat('/serviceProviders/'),
-}
